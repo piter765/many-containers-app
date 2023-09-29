b330baac858fc34d70ae85019f14d53e44b0b062
piter765 <piotrektymula1@gmail.com>
2023-09-29 11:30:49 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to React</p>
+          <Link to="/">Home</Link>
+          <Link to="/otherPage">Other page</Link>
+        </header>
+          <div>
+            <Route exact path="/" component={Fib}/>
+            <Route path="/otherpage" component={OtherPage}/>
+          </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/keys.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+}
